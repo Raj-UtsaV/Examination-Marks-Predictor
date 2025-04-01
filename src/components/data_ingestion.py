d37b41a24b4445864409d52bc2c5b5b23ddab149
Raj-UtsaV <utsavraj911@outlook.com>
2025-04-02 01:10:08 +0530

    Fix typos and improve data handling in model training components

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -2,14 +2,11 @@ import os
 import sys
 from src.exception import CustomException
 from src.logger import logging
-
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConf
-
 from src.components.model_trainer import ModelTrainer
 from src.components.model_trainer import ModelTrainerConfig 
 
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,7 +1,6 @@
 import os
 import sys
 from dataclasses import dataclass
-
 from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,
@@ -13,10 +12,8 @@ from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
-
 from src.exception import CustomException
 from src.logger import logging
-
 from src.utils import save_object,evaluate_models
 
 @dataclass
@@ -27,7 +24,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
         
-    def initiate_mode_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self,train_array,test_array):
         try:
             logging.info("Split training and test input data")
             X_train,y_train,X_test,y_test=(
