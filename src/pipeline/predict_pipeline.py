61d8902b929a32781248d9a09fd933612681aec2
Raj-UtsaV <utsavraj911@outlook.com>
2025-04-02 20:57:09 +0530

    Refactor pipeline structure and add Flask integration for prediction functionality

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
 from src.exception import CustomException
 from src.utils import load_object
 
@@ -11,15 +12,15 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            model_path=os.path.join("artifacts","model.pkl")
-            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join(".artifacts","model.pkl")
+            preprocessor_path=os.path.join('.artifacts','preprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
-            return preds
+            return np.round(preds,2)
         
         except Exception as e:
             raise CustomException(e,sys)
